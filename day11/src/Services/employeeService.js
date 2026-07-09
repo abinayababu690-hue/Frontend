441d1b9bb4e383fcf1926528b44054fdcc5b5ddf
ABINAYA B <abinayababu690@gmail.com>
2026-07-09 21:48:58 +0530

    Added Day11 project

---
+++ day11/src/Services/employeeService.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+//API URL
+
+const API_URL="https://6a4b36b7f5eab0bb6b6258b6.mockapi.io/Employees";
+//GET EMPLOYEES
+ 
+export const getEmployees=()=>{
+
+    return axios.get(API_URL);
+};
+//POST 
+
+export const addEmployee=(employee)=>{
+    return axios.post(API_URL,employee);
+};
+
+//PUT-->https://6a48e677a033dcb98d65067b.mockapi.io/employees/id
+export const updateEmployee=(id,employee)=>{
+    return axios.put(`${API_URL}/${id}`,employee);
+};
+
+//DELETE->https://6a48e677a033dcb98d65067b.mockapi.io/employees/id
+
+export const deleteEmployee=(id)=>{
+    return axios.delete(`${API_URL}/${id}`);
+};
